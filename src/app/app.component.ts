6e462a4597a39c39d4131eff10aff4472defc19d
hemmoleg <hemmoleg@gmail.com>
2023-10-29 17:04:42 +0100

    formated some files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit, ViewChildren, QueryList, ViewChild } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChildren,
+  QueryList,
+  ViewChild,
+} from '@angular/core';
 import { CellData, CellStatus } from '../cellData';
 import { RowData } from '../rowData';
 import { Solver } from '../solver';
@@ -14,155 +20,160 @@ import { GameComponent } from './game/game.component';
   selector: 'app-root',
   styleUrls: ['./app.component.scss', './cell/cell.component.scss'],
   template: `
-        <div id="master">
-            <game *ngFor="let riddle of riddlesJSON.riddles, let i = index"
-                [rowInputs]="riddle.rowInputs"
-                [columnInputs]="riddle.columnInputs"
-                [index]="i"
-                [centerIndex]="centerRiddleIndex"
-                (matrixSet)="onMatrixSet()"
-                ></game>
-        </div>
+    <div id="master">
+      <game
+        *ngFor="let riddle of riddlesJSON.riddles; let i = index"
+        [rowInputs]="riddle.rowInputs"
+        [columnInputs]="riddle.columnInputs"
+        [index]="i"
+        [centerIndex]="centerRiddleIndex"
+        (matrixSet)="onMatrixSet()"
+      ></game>
+    </div>
 
-        <cell id="testPos" [cellData]="TESTcellData">
-        </cell>
-        <button (click)="testCell()" style="margin-top:40px">test</button><br>
+    <!-- <cell id="testPos" [cellData]="TESTcellData">
+        </cell> -->
+    <!-- <button (click)="testCell()" style="margin-top:40px">test</button><br> -->
 
-        <!-- <div stlye="display:flex; flex-wrap: wrap">
+    <!-- <div stlye="display:flex; flex-wrap: wrap">
             <cell *ngFor="let cd of TESTCellDataArray"
                 [cellData]="cd">
             </cell>
         </div> -->
 
-        <button (click)="btnSolveClicked()">CALC</button><br>
-        <button (click)="btnMovePlusClicked()">move+</button><br>
-        <button (click)="btnMoveMinusClicked()">move-</button>
+    <button (click)="btnSolveClicked()">CALC</button><br />
+    <button (click)="btnMovePlusClicked()">move+</button><br />
+    <button (click)="btnMoveMinusClicked()">move-</button>
 
-        <!-- 'result animation' is played in this matrix -->
-        <matrix #matrixComponent
-            [isEditable]="false"
-            [isResult]="true"
-            (matrixSet)="onMatrixSet()">
+    <!-- 'result animation' is played in this matrix -->
+    <matrix
+      #matrixComponent
+      [isEditable]="false"
+      [isResult]="true"
+      (matrixSet)="onMatrixSet()"
+    >
+    </matrix>
+
+    <!-- would show each resultMatrix -> each step of the solution -->
+    <div *ngIf="resultMatrices">
+      <div
+        *ngFor="let resultMatrix of resultMatrices.resultMatrices | slice : 1; let i = index">
+        <matrix [rowDatas]="resultMatrix" [isEditable]="true" [isResult]="true">
         </matrix>
-
-        <!-- would show each resultMatrix -> each step of the solution -->
-        <!-- <div *ngFor="let resultMatrix of resultMatrices.resultMatrices | slice: 1, let i = index">
-            Iteration: {{i+1}}
-            <matrix
-                [rowDatas]="resultMatrix"
-                [isEditable]="true"
-                [isResult]="true">
-            </matrix>
-        </div> -->
-  `
+      </div>
+    </div>
+  `,
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
+  matrixX: number;
+  matrixY: number;
+  rowDatas: RowData[] = [];
+  displayRows: RowData[] = [];
 
-    matrixX: number;
-    matrixY: number;
-    rowDatas: RowData[] = [];
-    displayRows: RowData[] = [];
+  TESTcellData: CellData = new CellData();
+  TESTCellDataArray: Array<CellData> = [];
+  resultMatrices: ResultMatrices;
+  currentResultMatrixIndex = 0;
 
-    TESTcellData: CellData = new CellData();
-    TESTCellDataArray: Array<CellData> = [];
-    resultMatrices: ResultMatrices;
-    currentResultMatrixIndex = 0;
+  centerRiddleIndex = 3;
+  solvedGameComponent: GameComponent;
+  showSteps = 500;
+  shownSteps = 0;
 
-    centerRiddleIndex = 3;
-    solvedGameComponent: GameComponent;
+  riddlesJSON;
 
-    riddlesJSON;
+  @ViewChild('matrixComponent') matrixComponent: MatrixComponent;
+  @ViewChildren(GameComponent) gameComponents: QueryList<GameComponent>;
 
-    @ViewChild('matrixComponent') matrixComponent: MatrixComponent;
-    @ViewChildren(GameComponent) gameComponents: QueryList<GameComponent>;
+  btnMovePlusClicked() {
+    this.centerRiddleIndex++;
+    if (this.centerRiddleIndex > this.riddlesJSON.riddles.length - 1)
+      this.centerRiddleIndex = this.riddlesJSON.riddles.length - 1;
+  }
 
-    btnMovePlusClicked()
-    {
-        this.centerRiddleIndex++;
-        if(this.centerRiddleIndex > (this.riddlesJSON.riddles.length - 1))
-            this.centerRiddleIndex = this.riddlesJSON.riddles.length - 1;
+  btnMoveMinusClicked() {
+    console.log(this.gameComponents);
+    this.centerRiddleIndex--;
+    if (this.centerRiddleIndex < 0) this.centerRiddleIndex = 0;
+  }
+
+  ngOnInit() {
+    this.TESTcellData.status = CellStatus.empty;
+
+    console.log(riddlesJSON);
+
+    this.riddlesJSON = riddlesJSON;
+    console.log(this.riddlesJSON);
+
+    for (let i = 0; i < 9; i++) this.TESTCellDataArray.push(new CellData());
+  }
+
+  async btnSolveClicked() {
+    const rowNumbers =
+      this.riddlesJSON.riddles[this.centerRiddleIndex].rowInputs;
+    const columnNumbers =
+      this.riddlesJSON.riddles[this.centerRiddleIndex].columnInputs;
+
+    console.log(rowNumbers, columnNumbers);
+
+    this.shownSteps = 0;
+    this.solvedGameComponent =
+      this.gameComponents.toArray()[this.centerRiddleIndex];
+
+    const solver = new Solver();
+    this.resultMatrices = solver.solve(rowNumbers, columnNumbers);
+
+    this.currentResultMatrixIndex = 0;
+    this.setNextResultMatrix();
+  }
+
+  async setNextResultMatrix(withDelay: boolean = false) {
+    if (!this.resultMatrices || this.currentResultMatrixIndex >= this.resultMatrices.length)
+      return;
+
+    if (withDelay) {
+      console.log('delay...');
+      //await this.delay(500);
     }
 
-    btnMoveMinusClicked()
-    {
-        console.log(this.gameComponents);
-        this.centerRiddleIndex--;
-        if(this.centerRiddleIndex < 0) this.centerRiddleIndex = 0;
-    }
+    console.log('setting matrix', this.currentResultMatrixIndex, 'of', this.resultMatrices.length - 1);
+    const resultRowDatas = this.resultMatrices.resultMatrices[this.currentResultMatrixIndex];
+    this.solvedGameComponent.setRowDatasAnimated(
+      resultRowDatas,
+      this.currentResultMatrixIndex % 2 == 1
+    );
+    this.currentResultMatrixIndex++;
+  }
 
-    ngOnInit()
-    {
-        this.TESTcellData.status = CellStatus.empty;
+  onMatrixSet() {
+    if (this.shownSteps + 1 < this.showSteps) {
+      this.shownSteps++;
+      this.setNextResultMatrix(true);
+    } else return;
+  }
 
-        console.log(riddlesJSON);
+  testCell() {
+    if (this.TESTcellData.status == CellStatus.empty)
+      this.TESTcellData.status = CellStatus.cross;
+    else this.TESTcellData.status = CellStatus.empty;
+  }
 
-        this.riddlesJSON = riddlesJSON;
-        console.log(this.riddlesJSON);
+  btnCreateClicked(dimensions: number[]) {
+    this.matrixX = dimensions[0];
+    this.matrixY = dimensions[1];
+    this.rowDatas = Array.apply(null, Array(dimensions[0])).map(
+      (value, i1) =>
+        (Array[i1] = new RowData(
+          Array.apply(null, Array(dimensions[1])).map(
+            (value, i2) => (Array[i2] = new CellData())
+          )
+        ))
+    );
 
-        for(let i = 0; i < 9; i++)
-            this.TESTCellDataArray.push(new CellData());
-    }
+    console.log(this.rowDatas);
+  }
 
-    async btnSolveClicked()
-    {
-        const rowNumbers = this.riddlesJSON.riddles[this.centerRiddleIndex].rowInputs;
-        const columnNumbers = this.riddlesJSON.riddles[this.centerRiddleIndex].columnInputs;
-
-        console.log(rowNumbers, columnNumbers);
-
-        this.solvedGameComponent = this.gameComponents.toArray()[this.centerRiddleIndex];
-
-        const solver = new Solver();
-        this.resultMatrices = solver.solve(rowNumbers, columnNumbers);
-
-        this.currentResultMatrixIndex = 0;
-        this.setNextResultMatrix();
-    }
-
-    async setNextResultMatrix(withDelay: boolean = false)
-    {
-        if(!this.resultMatrices || this.currentResultMatrixIndex >= this.resultMatrices.length)
-            return;
-
-        if(withDelay)
-        {
-            console.log('delay...');
-            //await this.delay(500);
-        }
-
-        console.log('setting matrix', this.currentResultMatrixIndex);
-        const resultRowDatas = this.resultMatrices.resultMatrices[this.currentResultMatrixIndex];
-        this.solvedGameComponent.setRowDatasAnimated(resultRowDatas, this.currentResultMatrixIndex % 2 == 1);
-        this.currentResultMatrixIndex++;
-    }
-
-    onMatrixSet()
-    {
-        this.setNextResultMatrix(true);
-    }
-
-    testCell()
-    {
-        if(this.TESTcellData.status == CellStatus.empty)
-            this.TESTcellData.status = CellStatus.cross;
-        else
-            this.TESTcellData.status = CellStatus.empty;
-    }
-
-    btnCreateClicked(dimensions: number[])
-    {
-        this.matrixX = dimensions[0];
-        this.matrixY = dimensions[1];
-        this.rowDatas = Array.apply(null, Array(dimensions[0])).map(
-            (value, i1) => Array[i1] = new RowData(Array.apply(null, Array(dimensions[1])).map((value, i2) => Array[i2] = new CellData()))
-        );
-
-        console.log(this.rowDatas);
-    }
-
-
-    async delay(ms: number)
-    {
-        return new Promise(resolve => setTimeout(resolve, ms));
-    }
+  async delay(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
 }
--- src/app/matrix/matrix.component.ts
+++ src/app/matrix/matrix.component.ts
@@ -3,287 +3,275 @@ import { CellData } from 'src/cellData';
 import { RowData } from './../../rowData';
 import { ClassField } from '@angular/compiler';
 
-const BetterArray = require('better-array')
+import BetterArray from 'better-array';
 
 @Component({
-    selector: 'matrix',
-    styleUrls: ['./matrix.component.scss'],
-    template: `
-        <div id="matrixContainer"
-            [class.isResult]="isResult"
-            [style.width.px]="calculatedWidth">
-            <cell *ngFor="let cd of cellDatas; let i = index"
-                [cellData]="cd"
-                [isFithRow]="cellIsFithRow(i)"
-                [isSixthRow]="cellIsSxithRow(i)"
-                [isFithColumn]="cellIsFithColumm(i)"
-                [isSixthColumn]="cellIsSixthColumm(i)"
-                [zIndex2]="glowOnIndices.contains(i) || glowOffIndices.contains(i)"
-                [glowOn]="glowOnIndices.contains(i)"
-                [glowOff]="glowOffIndices.contains(i)"
-                [rowFirst]="!setByColumn && i == applyClipIndices.first()"
-                [row]="!setByColumn && i != applyClipIndices.first() && i != applyClipIndices.last() && applyClipIndices.contains(i)"
-                [rowLast]="!setByColumn && i == applyClipIndices.last()"
-                [columnFirst]="setByColumn && i == applyClipIndices.first()"
-                [column]="setByColumn && i != applyClipIndices.first() && i != applyClipIndices.last() && applyClipIndices.contains(i)"
-                [columnLast]="setByColumn && i == applyClipIndices.last()"
-                (CellGlowAnimationEnd)="onCellGlowAnimationEnd()"
-                (CellContentAnimationEnd)="onCellAnimationEnd()">
-            </cell>
-        </div>
-    `
+  selector: 'matrix',
+  styleUrls: ['./matrix.component.scss'],
+  template: `
+    <!-- [style.width.px]="calculatedWidth" -->
+    <div
+      id="matrixContainer"
+      [class.isResult]="isResult"
+      [style.width.px]="calculatedWidth"
+    >
+      <cell
+        *ngFor="let cd of cellDatas; let i = index"
+        [cellData]="cd"
+        [isFithRow]="cellIsFithRow(i)"
+        [isSixthRow]="cellIsSxithRow(i)"
+        [isFithColumn]="cellIsFithColumm(i)"
+        [isSixthColumn]="cellIsSixthColumm(i)"
+        [zIndex2]="glowOnIndices.contains(i) || glowOffIndices.contains(i)"
+        [glowOn]="glowOnIndices.contains(i)"
+        [glowOff]="glowOffIndices.contains(i)"
+        [rowFirst]="!setByColumn && i == applyClipIndices.first()"
+        [row]="
+          !setByColumn &&
+          i != applyClipIndices.first() &&
+          i != applyClipIndices.last() &&
+          applyClipIndices.contains(i)
+        "
+        [rowLast]="!setByColumn && i == applyClipIndices.last()"
+        [columnFirst]="setByColumn && i == applyClipIndices.first()"
+        [column]="
+          setByColumn &&
+          i != applyClipIndices.first() &&
+          i != applyClipIndices.last() &&
+          applyClipIndices.contains(i)
+        "
+        [columnLast]="setByColumn && i == applyClipIndices.last()"
+        (CellGlowAnimationEnd)="onCellGlowAnimationEnd()"
+        (CellContentAnimationEnd)="onCellAnimationEnd()"
+      >
+      </cell>
+    </div>
+  `,
 })
-export class MatrixComponent{
+export class MatrixComponent {
+  cellDatas: CellData[];
+  calculatedWidth: number;
+  cellsPerRow: number;
+  cellsPerColumn: number;
+  @Input() set displayRowDatas(datas: RowData[]) {
+    this.cellDatas = [];
+    datas.forEach(
+      (data) => (this.cellDatas = this.cellDatas.concat(data.cellDatas))
+    );
 
-    cellDatas: CellData[];
-    calculatedWidth: number;
-    cellsPerRow: number;
-    cellsPerColumn: number;
-    @Input() set displayRowDatas(datas: RowData[])
-    {
-        this.cellDatas = [];
-        datas.forEach(data => this.cellDatas = this.cellDatas.concat(data.cellDatas));
+    this.calculatedWidth =
+      datas[0].cellDatas.length * 32 + datas[0].cellDatas.length * 4;
 
-        this.calculatedWidth = datas[0].cellDatas.length * 32 + datas[0].cellDatas.length * 4;
+    this.cellsPerRow = datas[0].cellDatas.length;
+    this.cellsPerColumn = this.cellDatas.length / this.cellsPerRow;
+  }
+  rowDatasToSet: RowData[] = [];
+  cellDatasToSet: CellData[];
+  setByColumn: boolean;
+  rowIndexToSet: number;
+  columnIndexToSet: number;
+  applyClipIndices = BetterArray([]);
+  glowOnIndices = BetterArray([]);
+  glowOffIndices = BetterArray([]);
+  glowingCells = 0;
 
-        this.cellsPerRow = datas[0].cellDatas.length;
-        this.cellsPerColumn = this.cellDatas.length / this.cellsPerRow;
-    };
-    rowDatasToSet: RowData[] = [];
-    cellDatasToSet: CellData[];
-    setByColumn: boolean;
-    rowIndexToSet: number;
-    columnIndexToSet: number;
-    applyClipIndices = BetterArray([]);
-    glowOnIndices = BetterArray([]);
-    glowOffIndices = BetterArray([]);
-    glowingCells = 0;
+  matrixIsSet = false;
 
-    matrixIsSet = false;
+  @Input() isEditable: boolean;
+  @Input() isResult: true;
+  @Output() matrixSet = new EventEmitter();
 
-    @Input() isEditable: boolean;
-    @Input() isResult: true;
-    @Output() matrixSet = new EventEmitter();
+  setRowDatasAnimated(inputRowDatas: RowData[], setByColumn: boolean) {
+    if (inputRowDatas.length == 0) return;
 
+    console.log('setRowDatasAnimated', inputRowDatas);
 
-    setRowDatasAnimated(inputRowDatas: RowData[], setByColumn: boolean)
-    {
-        if(inputRowDatas.length == 0) return;
+    this.rowDatasToSet = inputRowDatas;
+    this.cellDatasToSet = [];
+    inputRowDatas.forEach(
+      (data) =>
+        (this.cellDatasToSet = this.cellDatasToSet.concat(data.cellDatas))
+    );
 
-        console.log('setRowDatasAnimated', inputRowDatas);
+    this.setByColumn = setByColumn;
 
-        this.rowDatasToSet = inputRowDatas;
-        this.cellDatasToSet = [];
-        inputRowDatas.forEach(data => this.cellDatasToSet = this.cellDatasToSet.concat(data.cellDatas));
-
-        this.setByColumn = setByColumn;
-
-        if(setByColumn){
-            this.rowIndexToSet = 0;
-            this.columnIndexToSet = 0;
-        }else{
-            this.rowIndexToSet = 0;
-            this.columnIndexToSet = 0;
-        }
-
-        this.matrixIsSet = false;
-
-        this.glowingCells = 0;
-        this.glowOnIndices = BetterArray([]);
-        this.glowOffIndices = BetterArray([]);
-        this.calcRowColumnIndexToSet();
-
-        //this.setNextCell();
+    if (setByColumn) {
+      this.rowIndexToSet = 0;
+      this.columnIndexToSet = 0;
+    } else {
+      this.rowIndexToSet = 0;
+      this.columnIndexToSet = 0;
     }
 
-    calcRowColumnIndexToSet()
-    {
-        if(this.setByColumn)
-        {
-            let isDiff = false;
-            do{
-                for(let col = this.columnIndexToSet; col < this.rowDatasToSet[0].cellDatas.length; col++)
-                {
-                    for(let row = 0; row < this.rowDatasToSet.length; row++)
-                    {
-                        const inputCell = this.cellDatasToSet[row * this.cellsPerRow + col];
-                        const cellToSet = this.cellDatas[row * this.cellsPerRow + col];
+    this.matrixIsSet = false;
 
-                        console.log('checking', col, row);
+    this.glowingCells = 0;
+    this.glowOnIndices = BetterArray([]);
+    this.glowOffIndices = BetterArray([]);
+    this.calcRowColumnIndexToSet();
 
-                        if(inputCell.status != cellToSet.status)
-                        {
-                            console.log('found dif');
-                            isDiff = true;
-                            this.columnIndexToSet = col;
-                            this.rowIndexToSet = row;
-                            break;
-                        }
-                    }
-                    if(isDiff) break;
-                }
-                if(!isDiff)
-                    this.matrixIsSet = true;
+    //this.setNextCell();
+  }
 
-            }while(!isDiff && !this.matrixIsSet);
-        }
-        else
-        {
-            let isDiff = false;
-            do{
-                for(let row = this.rowIndexToSet; row < this.rowDatasToSet.length; row++)
-                {
-                    for(let col = 0; col < this.rowDatasToSet[0].cellDatas.length; col++)
-                    {
-                        const inputCell = this.cellDatasToSet[row * this.cellsPerRow + col];
-                        const cellToSet = this.cellDatas[row * this.cellsPerRow + col];
+  calcRowColumnIndexToSet() {
+    if (this.setByColumn) {
+      let isDiff = false;
+      do {
+        for (
+          let col = this.columnIndexToSet;
+          col < this.rowDatasToSet[0].cellDatas.length;
+          col++
+        ) {
+          for (let row = 0; row < this.rowDatasToSet.length; row++) {
+            const inputCell = this.cellDatasToSet[row * this.cellsPerRow + col];
+            const cellToSet = this.cellDatas[row * this.cellsPerRow + col];
 
-                        if(inputCell.status != cellToSet.status)
-                        {
-                            isDiff = true;
-                            this.columnIndexToSet = col;
-                            this.rowIndexToSet = row;
-                            break;
-                        }
-                    }
-                    if(isDiff) break;
-                }
-                if(!isDiff)
-                    this.matrixIsSet = true;
+            //console.log('checking', col, row);
 
-            }while(!isDiff && !this.matrixIsSet);
-        }
-
-        if(this.matrixIsSet)
-        {
-            console.log('matrix is set');
-            this.setGlowOffIndices();
-            return;
-        }
-
-        const cellIndexToSet = this.rowIndexToSet * this.cellsPerRow + this.columnIndexToSet;
-        if(!this.glowOnIndices.contains(cellIndexToSet))
-        {
-            if(this.glowOnIndices.size() > 0)
-            {
-                this.setGlowOffIndices();
-                return;
+            if (inputCell.status != cellToSet.status) {
+              //console.log('found dif');
+              isDiff = true;
+              this.columnIndexToSet = col;
+              this.rowIndexToSet = row;
+              break;
             }
-            else
-            {
-                this.setGlowOnIndices();
-                return;
+          }
+          if (isDiff) break;
+        }
+        if (!isDiff) this.matrixIsSet = true;
+      } while (!isDiff && !this.matrixIsSet);
+    } else {
+      let isDiff = false;
+      do {
+        for (
+          let row = this.rowIndexToSet;
+          row < this.rowDatasToSet.length;
+          row++
+        ) {
+          for (
+            let col = 0;
+            col < this.rowDatasToSet[0].cellDatas.length;
+            col++
+          ) {
+            const inputCell = this.cellDatasToSet[row * this.cellsPerRow + col];
+            const cellToSet = this.cellDatas[row * this.cellsPerRow + col];
+
+            if (inputCell.status != cellToSet.status) {
+              isDiff = true;
+              this.columnIndexToSet = col;
+              this.rowIndexToSet = row;
+              break;
             }
+          }
+          if (isDiff) break;
         }
-        else{
-            this.setCell();
+        if (!isDiff) this.matrixIsSet = true;
+      } while (!isDiff && !this.matrixIsSet);
+    }
+
+    if (this.matrixIsSet) {
+      console.log('matrix is set');
+      this.setGlowOffIndices();
+      return;
+    }
+
+    const cellIndexToSet =
+      this.rowIndexToSet * this.cellsPerRow + this.columnIndexToSet;
+    if (!this.glowOnIndices.contains(cellIndexToSet)) {
+      if (this.glowOnIndices.size() > 0) {
+        this.setGlowOffIndices();
+        return;
+      } else {
+        this.setGlowOnIndices();
+        return;
+      }
+    } else {
+      this.setCell();
+    }
+  }
+
+  setCell() {
+    const inputCell =
+      this.cellDatasToSet[
+        this.rowIndexToSet * this.cellsPerRow + this.columnIndexToSet
+      ];
+    const cellToSet =
+      this.cellDatas[
+        this.rowIndexToSet * this.cellsPerRow + this.columnIndexToSet
+      ];
+    cellToSet.status = inputCell.status;
+  }
+
+  onCellAnimationEnd() {
+    //console.log('onCellAnimationEnd');
+    this.calcRowColumnIndexToSet();
+  }
+
+  onCellGlowAnimationEnd() {
+    if (this.glowOnIndices.size() > 0) {
+      this.glowingCells++;
+      //console.log(this.glowingCells, this.glowOnIndices.size());
+
+      if (this.glowingCells == this.glowOnIndices.size()) {
+        this.setCell();
+      }
+    } else {
+      this.glowingCells--;
+      if (this.glowingCells == 0) {
+        if (this.matrixIsSet) {
+          this.applyClipIndices = BetterArray([]);
+          this.matrixSet.emit();
+          return;
+        } else {
+          this.setGlowOnIndices();
         }
+      }
     }
+  }
 
-    setCell()
-    {
-        const inputCell = this.cellDatasToSet[this.rowIndexToSet * this.cellsPerRow + this.columnIndexToSet];
-        const cellToSet = this.cellDatas[this.rowIndexToSet * this.cellsPerRow + this.columnIndexToSet];
-        cellToSet.status = inputCell.status;
+  setGlowOnIndices() {
+    if (!this.setByColumn) {
+      for (let i = 1; i <= this.cellsPerRow; i++)
+        this.glowOnIndices.$push(i + this.rowIndexToSet * this.cellsPerRow - 1);
+    } else {
+      for (let i = 0; i < this.cellDatasToSet.length / this.cellsPerRow; i++) {
+        this.glowOnIndices.$push(this.columnIndexToSet + i * this.cellsPerRow);
+      }
     }
+    this.applyClipIndices = this.glowOnIndices;
+  }
 
-    onCellAnimationEnd()
-    {
-        //console.log('onCellAnimationEnd');
-        this.calcRowColumnIndexToSet();
-    }
+  setGlowOffIndices() {
+    this.glowOffIndices = this.glowOnIndices;
+    this.glowOnIndices = BetterArray([]);
+  }
 
-    onCellGlowAnimationEnd()
-    {
-        if(this.glowOnIndices.size() > 0)
-        {
-            this.glowingCells++;
-            //console.log(this.glowingCells, this.glowOnIndices.size());
+  cellIsFithRow(i: number) {
+    const row = Math.floor(i / this.cellsPerRow);
+    if ((row + 1) % 5 == 0 && i < this.cellDatas.length - this.cellsPerRow)
+      return true;
+  }
 
-            if(this.glowingCells == this.glowOnIndices.size())
-            {
-                this.setCell();
-            }
-        }
-        else
-        {
-            this.glowingCells--;
-            if(this.glowingCells == 0)
-            {
-                if(this.matrixIsSet)
-                {
-                    this.applyClipIndices = BetterArray([]);
-                    this.matrixSet.emit();
-                    return;
-                }
-                else
-                {
-                    this.setGlowOnIndices();
-                }
-            }
-        }
-    }
+  cellIsSxithRow(i: number) {
+    const row = Math.floor(i / this.cellsPerRow);
+    if (row % 5 == 0 && i > this.cellsPerRow) return true;
+  }
 
-    setGlowOnIndices()
-    {
-        if(!this.setByColumn)
-        {
-            for(let i = 1; i <= this.cellsPerRow; i++)
-                this.glowOnIndices.$push(i + (this.rowIndexToSet * this.cellsPerRow) - 1);
-        }
-        else
-        {
-            for(let i = 0; i < this.cellDatasToSet.length / this.cellsPerRow; i++)
-            {
-                this.glowOnIndices.$push(this.columnIndexToSet + (i * this.cellsPerRow))
-            }
-        }
-        this.applyClipIndices = this.glowOnIndices;
-    }
+  cellIsFithColumm(i: number) {
+    const row = Math.floor(i / this.cellsPerRow);
+    if ((i + 1) % 5 == 0 && i < (row + 1) * this.cellsPerRow - 1) return true;
+  }
 
-    setGlowOffIndices()
-    {
-        this.glowOffIndices = this.glowOnIndices;
-        this.glowOnIndices = BetterArray([]);
-    }
+  cellIsSixthColumm(i: number) {
+    const row = Math.floor(i / this.cellsPerRow);
+    if (i % 5 == 0 && i > row * this.cellsPerRow) return true;
+  }
 
-    cellIsFithRow(i: number)
-    {
-        const row = Math.floor(i / this.cellsPerRow);
-        if((row + 1) % 5 == 0 && i < this.cellDatas.length - this.cellsPerRow)
-            return true;
-    }
+  cellIsFithColumn(i: number) {
+    if ((i + 1) % 5 == 0) return true;
+  }
 
-    cellIsSxithRow(i: number)
-    {
-        const row = Math.floor(i / this.cellsPerRow);
-        if(row % 5 == 0 && i > this.cellsPerRow)
-            return true;
-    }
-
-    cellIsFithColumm(i: number)
-    {
-        const row = Math.floor(i / this.cellsPerRow);
-        if((i + 1) % 5 == 0 && i < (((row + 1) * this.cellsPerRow) - 1) )
-            return true;
-    }
-
-    cellIsSixthColumm(i: number)
-    {
-        const row = Math.floor(i / this.cellsPerRow);
-        if(i % 5 == 0 && i > row * this.cellsPerRow )
-            return true;
-    }
-
-    cellIsFithColumn(i: number)
-    {
-        if((i + 1) % 5 == 0)
-            return true;
-    }
-
-    async delay(ms: number)
-    {
-        return new Promise(resolve => setTimeout(resolve, ms));
-    }
-
+  async delay(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
 }
